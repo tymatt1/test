f0e9c348e3dd62b592e16ea854a21f33f1eea1f1
tymatt1 <139138324+tymatt1@users.noreply.github.com>
2023-07-14 11:23:09 -0400

    Add files via upload
    Create a customizable pyramid of stars

---
+++ loop.py
@@ -0,0 +1,16 @@
+input = int(input("Enter the number of rows you want: "))
+print("\n")
+i = 0
+star = "*       "
+
+while i < input + 1:
+
+    starNum = star * i
+    starNum = starNum.center(125)
+    print(starNum)
+    starNum = "*       "
+    i += 1
+
+print("\nDone")
+
+
